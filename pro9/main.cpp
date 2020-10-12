9fbb81e0e21f28d0e56165adb1a1a13bd87953f1
SeungWookHan <hswook12@me.com>
2020-10-12 16:53:59 +0900

    Add Pro9

---
+++ pro9/main.cpp
@@ -0,0 +1,74 @@
+#include <opencv2/opencv.hpp>
+#include "opencv2/video/tracking.hpp"
+using namespace cv;
+using namespace std;
+
+void calc_Histo(const Mat &image, Mat &hist, int bins, int range_max = 256)
+{
+    int histSize[] = {bins};               // ������׷� ��� ����
+    float range[] = {0, (float)range_max}; // ������׷� ����
+    int channels[] = {0};                  // ä�� ���
+    int dims = image.channels();
+    ;
+    const float *ranges[] = {range};
+
+    calcHist(&image, 1, channels, Mat(), hist, dims, histSize, ranges);
+}
+
+// hue ä�ο� ���� ���� �ȷ�Ʈ ��� ����
+Mat make_palatte(int rows)
+{
+    Mat hsv(rows, 1, CV_8UC3);
+    for (int i = 0; i < rows; i++)
+    {
+        uchar hue = saturate_cast<uchar>((float)i / rows * 180);
+        hsv.at<Vec3b>(i) = Vec3b(hue, 255, 255);
+    }
+    cvtColor(hsv, hsv, COLOR_HSV2BGR);
+    return hsv;
+}
+
+void draw_hist_hue(Mat hist, Mat &hist_img, Size size = Size(256, 200))
+{
+    Mat hsv_palatte = make_palatte(hist.rows);
+
+    hist_img = Mat(size, CV_8UC3, Scalar(255, 255, 255));
+    float bin = (float)hist_img.cols / hist.rows;
+    normalize(hist, hist, 0, hist_img.rows, NORM_MINMAX);
+
+    for (int i = 0; i < hist.rows; i++)
+    {
+        float start_x = (i * bin);
+        float end_x = (i + 1) * bin;
+        Point2f pt1(start_x, 0);
+        Point2f pt2(end_x, hist.at<float>(i));
+
+        Scalar color = hsv_palatte.at<Vec3b>(i);
+        rectangle(hist_img, pt1, pt2, color, -1);
+    }
+    flip(hist_img, hist_img, 0);
+}
+
+int main()
+{
+    Mat image = imread("/Users/Han/programming/opencv/pro9/color_space.jpg", 1);
+    CV_Assert(!image.empty());
+
+    // imshow("연습문제 17", image);
+
+    Rect2d roi_img = selectROI(image); // Select ROI
+    Mat imCrop = image(roi_img);       // Crop image
+    imshow("선택영역", imCrop);
+
+    Mat HSV_img, HSV_arr[3];
+    cvtColor(imCrop, HSV_img, COLOR_BGR2HSV);
+    split(HSV_img, HSV_arr);
+
+    Mat hist_hue, hist_hue_img;
+    calc_Histo(HSV_arr[0], hist_hue, 18, 180);
+    draw_hist_hue(hist_hue, hist_hue_img, Size(360, 200));
+
+    imshow("Histogram", hist_hue_img);
+    waitKey();
+    return 0;
+}
